4f08c0b7b496f63c5d39ed4dbc4f0d86c0562e74
Timo Wingender <github@timo-wingender.de>
2015-01-08 15:46:12 +0100

    Revert "tpuarts: ignore received messages after sending" This change is not part of this branch
    This reverts commit 11921ca05259811779de1d7f4046bfcfa4c17c51.

--- src/backend/tpuartserial.cpp
+++ src/backend/tpuartserial.cpp
@@ -348,7 +348,7 @@ TPUARTSerialLayer2Driver::Run (pth_sem_t * stop1)
 	}
       while (in () > 0)
 	{
-	  if (in[0] == 0x8B) // L_DataConfirm positive
+	  if (in[0] == 0x8B)
 	    {
 	      if (!mode && vmode)
 		{
@@ -364,7 +364,7 @@ TPUARTSerialLayer2Driver::Run (pth_sem_t * stop1)
 		}
 	      in.deletepart (0, 1);
 	    }
-	  else if (in[0] == 0x0B) // L_DataConfirm negative
+	  else if (in[0] == 0x0B)
 	    {
 	      if (!mode && vmode)
 		{
@@ -386,7 +386,7 @@ TPUARTSerialLayer2Driver::Run (pth_sem_t * stop1)
 		}
 	      in.deletepart (0, 1);
 	    }
-	  else if ((in[0] & 0x07) == 0x07) // Reset-Indication
+	  else if ((in[0] & 0x07) == 0x07)
 	    {
 	      TRACEPRINTF (t, 0, this, "RecvWatchdog: %02X", in[0]);
 	      watch = 2;
@@ -394,17 +394,12 @@ TPUARTSerialLayer2Driver::Run (pth_sem_t * stop1)
 			 pth_time (10, 0));
 	      in.deletepart (0, 1);
 	    }
-          /*
-           * 0xCC acknowledge frame
-           * 0x0C NotAcknowledge frame
-           * 0xC0 Busy Frame
-           */
 	  else if (in[0] == 0xCC || in[0] == 0xC0 || in[0] == 0x0C)
 	    {
 	      RecvLPDU (in.array (), 1);
 	      in.deletepart (0, 1);
 	    }
-	  else if ((in[0] & 0xD0) == 0x90) //L_DATA.ind 
+	  else if ((in[0] & 0xD0) == 0x90)
 	    {
 	      if (in () < 6)
 		{
@@ -420,7 +415,7 @@ TPUARTSerialLayer2Driver::Run (pth_sem_t * stop1)
 		  in.deletepart (0, 1);
 		  continue;
 		}
-	      if (!acked && !waitconfirm)
+	      if (!acked)
 		{
 		  uchar c = 0x10;
 		  if ((in[5] & 0x80) == 0)
@@ -461,14 +456,11 @@ TPUARTSerialLayer2Driver::Run (pth_sem_t * stop1)
 		  in.deletepart (0, 1);
 		  continue;
 		}
-              if (!waitconfirm)
-                {
-	          acked = 0;
-	          RecvLPDU (in.array (), len);
-                }
+	      acked = 0;
+	      RecvLPDU (in.array (), len);
 	      in.deletepart (0, len);
 	    }
-	  else if ((in[0] & 0xD0) == 0x10) //L_LONG_DATA.ind 
+	  else if ((in[0] & 0xD0) == 0x10)
 	    {
 	      if (in () < 7)
 		{
@@ -484,7 +476,7 @@ TPUARTSerialLayer2Driver::Run (pth_sem_t * stop1)
 		  in.deletepart (0, 1);
 		  continue;
 		}
-	      if (!acked  && !waitconfirm)
+	      if (!acked)
 		{
 		  uchar c = 0x10;
 		  if ((in[1] & 0x80) == 0)
@@ -525,11 +517,8 @@ TPUARTSerialLayer2Driver::Run (pth_sem_t * stop1)
 		  in.deletepart (0, 1);
 		  continue;
 		}
-              if (!waitconfirm)
-                {
-	          acked = 0;
-	          RecvLPDU (in.array (), len);
-                }
+	      acked = 0;
+	      RecvLPDU (in.array (), len);
 	      in.deletepart (0, len);
 	    }
 	  else
