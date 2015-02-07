8c6cf2d3f41be904af9a6b8ad5685842eaf7907d
Ole Krüger <ole.krueger@campus.tu-berlin.de>
2015-02-07 21:38:27 +0100

    Apply code conventions with astyle

--- src/common/my_strings.h
+++ src/common/my_strings.h
@@ -23,140 +23,133 @@
 #include <string.h>
 
 /** implements a string */
-class String
-{
-  /** content */
-  char *data;
-  /** length */
-  unsigned len;
+class String {
+	/** content */
+	char* data;
+
+	/** length */
+	unsigned len;
+
 public:
-  /** free memory */
-    virtual ~ String ()
-  {
-    if (data)
-      delete[]data;
-  }
+	/** free memory */
+	virtual ~String() {
+		if (data)
+			delete[]data;
+	}
 
-  /** initialize with the empty string */
-  String ()
-  {
-    data = 0;
-    len = 0;
-  }
+	/** initialize with the empty string */
+	String() {
+		data = 0;
+		len = 0;
+	}
 
-  /** initialize with a string */
-  String (const String & a)
-  {
-    len = a.len;
-    if (len)
-      {
-	data = new char[len];
-	memcpy (data, a.data, len);
-      }
-    else
-      data = 0;
-  }
-  /** assign a String */
-  const String & operator = (const String & a)
-  {
-    if (data)
-      delete[]data;
-    len = a.len;
-    if (len)
-      {
-	data = new char[len];
-	memcpy (data, a.data, len);
-      }
-    else
-      data = 0;
-    return a;
-  }
+	/** initialize with a string */
+	String(const String& a) {
+		len = a.len;
 
-  /** initialize with a character constant */
-  String (const char *msg)
-  {
-    if (msg)
-      {
-	len = strlen (msg) + 1;
-	data = new char[len];
-	strcpy (data, msg);
-      }
-    else
-      {
-	data = 0;
-	len = 0;
-      }
-  }
+		if (len) {
+			data = new char[len];
+			memcpy(data, a.data, len);
+		} else
+			data = 0;
+	}
 
-  /** assign a character constant */
-  const String & operator = (const char *msg)
-  {
-    if (data)
-      delete[]data;
-    if (msg)
-      {
-	len = strlen (msg) + 1;
-	data = new char[len];
-	strcpy (data, msg);
-      }
-    else
-      {
-	data = 0;
-	len = 0;
-      }
-    return *this;
-  }
+	/** assign a String */
+	const String& operator =(const String& a) {
+		if (data)
+			delete[]data;
 
-  /** concats two strings*/
-  String operator + (const String & a)
-  {
-    String b;
-    if (!len)
-      return a;
-    if (!a.len)
-      return *this;
-    b.len = a.len + len - 1;
-    if (b.len > 0)
-      {
-	b.data = new char[b.len];
-	if (data)
-	  strcpy (b.data, data);
-	else
-	  b.data[0] = 0;
-	if (a.data)
-	  strcat (b.data, a.data);
-      }
-    return b;
-  }
+		len = a.len;
 
-  bool operator == (const String & a) const
-  {
-    if (!a.len && !len)
-      return 1;
-    if (a.len != len)
-      return 0;
-    return (!strcmp (data, a.data));
-  }
+		if (len) {
+			data = new char[len];
+			memcpy(data, a.data, len);
+		} else
+			data = 0;
 
-  bool operator!= (const String & a) const
-  {
-    return !(*this == a);
-  }
+		return a;
+	}
 
-  /** returns the content as char* */
-  const char *operator  () () const
-  {
-    return data;
-  }
+	/** initialize with a character constant */
+	String(const char* msg) {
+		if (msg) {
+			len = strlen(msg) + 1;
+			data = new char[len];
+			strcpy(data, msg);
+		} else {
+			data = 0;
+			len = 0;
+		}
+	}
 
+	/** assign a character constant */
+	const String& operator =(const char* msg) {
+		if (data)
+			delete[]data;
+
+		if (msg) {
+			len = strlen(msg) + 1;
+			data = new char[len];
+			strcpy(data, msg);
+		} else {
+			data = 0;
+			len = 0;
+		}
+
+		return *this;
+	}
+
+	/** concats two strings*/
+	String operator +(const String& a) {
+		String b;
+
+		if (!len)
+			return a;
+
+		if (!a.len)
+			return *this;
+
+		b.len = a.len + len - 1;
+
+		if (b.len > 0) {
+			b.data = new char[b.len];
+
+			if (data)
+				strcpy(b.data, data);
+			else
+				b.data[0] = 0;
+
+			if (a.data)
+				strcat(b.data, a.data);
+		}
+
+		return b;
+	}
+
+	bool operator ==(const String& a) const {
+		if (!a.len && !len)
+			return 1;
+
+		if (a.len != len)
+			return 0;
+
+		return (!strcmp(data, a.data));
+	}
+
+	bool operator !=(const String& a) const {
+		return !(*this == a);
+	}
+
+	/** returns the content as char* */
+	const char* operator ()() const {
+		return data;
+	}
 };
 
 /** adds a text to a string */
-inline const String &
-operator += (String & a, const String b)
-{
-  String c = a + b;
-  return (a = c);
+inline const String& operator +=(String& a, const String b) {
+	String c = a + b;
+	return (a = c);
 }
 
 #endif
