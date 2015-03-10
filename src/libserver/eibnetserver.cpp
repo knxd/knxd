e6c4964a67bd555236244d819e82e3a1dca190a2
Matthias Urlichs <matthias@urlichs.de>
2015-03-10 22:55:07 +0000

    Added an error message
    Much more is missing …

--- src/libserver/eibnetserver.cpp
+++ src/libserver/eibnetserver.cpp
@@ -43,6 +43,7 @@ EIBnetServer::EIBnetServer (const char *multicastaddr, int port, bool Tunnel,
 
   if (GetHostIP (&maddr, multicastaddr) == 0)
     {
+      ERRORPRINTF (t, 8, this, "Addr '%s' not resolvable", multicastaddr);
       sock = 0;
       return;
     }
