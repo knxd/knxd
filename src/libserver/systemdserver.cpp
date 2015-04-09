646584dc2063a0e42685f2d91faff2664bbe7af4
Marc Joliet <marcec@gmx.de>
2015-04-09 21:48:12 +0200

    Clean up the included headers

--- src/libserver/systemdserver.cpp
+++ src/libserver/systemdserver.cpp
@@ -17,9 +17,9 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include <stdio.h>
-#include <sys/un.h>
+#include <sys/socket.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <systemd/sd-daemon.h>
 #include "systemdserver.h"
 
