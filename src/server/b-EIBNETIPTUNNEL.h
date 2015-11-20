4016095d244f6f878e2a7181b7a267d1c9bdd0f8
Matthias Urlichs <matthias@urlichs.de>
2015-11-20 15:48:02 +0100

    update help texts a bit

--- src/server/b-EIBNETIPTUNNEL.h
+++ src/server/b-EIBNETIPTUNNEL.h
@@ -24,13 +24,13 @@
 #include "eibnettunnel.h"
 
 #define EIBNETIPTUNNEL_URL "ipt:router-ip[:dest-port[:src-port[:nat-ip[:data-port]]]]]\n"
-#define EIBNETIPTUNNEL_DOC "ipt connects with the EIBnet/IP Tunneling protocol over an EIBnet/IP gateway. The gateway must be configured to route the necessary addresses\n\n"
+#define EIBNETIPTUNNEL_DOC "ipt directly connects to an EIBnet/IP gateway. The gateway must be configured to route the necessary addresses\n\n"
 
 #define EIBNETIPTUNNEL_PREFIX "ipt"
 #define EIBNETIPTUNNEL_CREATE eibnetiptunnel_Create
 
 #define EIBNETIPTUNNELNAT_URL "iptn:router-ip[:dest-port[:src-port]]\n"
-#define EIBNETIPTUNNELNAT_DOC "iptn connects with the EIBnet/IP Tunneling protocol over an EIBnet/IP gateway using NAT mode\n\n"
+#define EIBNETIPTUNNELNAT_DOC "iptn connects to an EIBnet/IP gateway using NAT mode\n\n"
 
 #define EIBNETIPTUNNELNAT_PREFIX "iptn"
 #define EIBNETIPTUNNELNAT_CREATE eibnetiptunnelnat_Create
