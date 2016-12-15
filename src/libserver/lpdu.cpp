8edb35d90af505342212f5bedd9243cbf9c53269
Matthias Urlichs <matthias@urlichs.de>
2016-12-15 13:33:37 +0100

    move common code off the loop

--- src/libserver/lpdu.cpp
+++ src/libserver/lpdu.cpp
@@ -252,11 +252,6 @@ L_Data_PDU::init (const CArray & c)
       if (len + 7 != c ())
 	return false;
       data.set (c.array () + 6, len);
-      c1 = 0;
-      for (i = 0; i < c () - 1; i++)
-	c1 ^= c[i];
-      c1 = ~c1;
-      valid_checksum = (c[c () - 1] == c1);
     }
   else
     {
@@ -282,13 +277,14 @@ L_Data_PDU::init (const CArray & c)
 	}
       else
 	data.set (c.array () + 7, len);
-
-      c1 = 0;
-      for (i = 0; i < c () - 1; i++)
-	c1 ^= c[i];
-      c1 = ~c1;
-      valid_checksum = (c[c () - 1] == c1);
     }
+
+  c1 = 0;
+  for (i = 0; i < c () - 1; i++)
+    c1 ^= c[i];
+  c1 = ~c1;
+  valid_checksum = (c[c () - 1] == c1);
+
   return true;
 }
 
