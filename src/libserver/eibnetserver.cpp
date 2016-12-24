44556e63054b3ccea5e79fe7cd7c0e3dc4040bff
Matthias Urlichs <matthias@urlichs.de>
2016-12-24 15:45:37 +0100

    fix: shared trace in eibnetserver

--- src/libserver/eibnetserver.cpp
+++ src/libserver/eibnetserver.cpp
@@ -361,7 +361,7 @@ EIBnetServer::addNAT (const L_Data_PDU & l)
 }
 
 ConnState::ConnState (EIBnetServer *p, eibaddr_t addr)
-  : Layer2mixin (TracePtr(new Trace(p->t,p->t->name+':'+FormatEIBAddr(addr))))
+  : Layer2mixin (TracePtr(new Trace(*(p->t),p->t->name+':'+FormatEIBAddr(addr))))
 {
   parent = p;
   timeout = pth_event (PTH_EVENT_RTIME, pth_time (120, 0));
