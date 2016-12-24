0376865e6b4ed4abb8f0b56f415f662bf608fe7b
Matthias Urlichs <matthias@urlichs.de>
2016-12-24 04:03:27 +0100

    added custom module for streaming w/ libev
    including a callback on errors
    
    not yet used

---
+++ src/common/iobuf.cpp
@@ -0,0 +1,123 @@
+/*
+    BCU SDK bcu development enviroment
+    Copyright (C) 2016 Matthias Urlichs <matthias@urlichs.de>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include <assert.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "iobuf.h"
+
+void
+SendBuf::io_cb (ev::io &w, int revents)
+{
+    while (sendbuf || !sendqueue.isempty()) {
+        if (sendbuf) {
+            int i = ::write(fd,sendbuf->data()+sendpos, sendbuf->size()-sendpos);
+            if (i > 0) {
+                sendpos += i;
+                if (sendpos < sendbuf->size())
+                    return;
+            } else {
+                if (i == 0 || errno != EAGAIN && errno != EWOULDBLOCK) {
+                    io.stop();
+                    on_error_cb();
+                }
+                return;
+            }
+        }
+
+        delete sendbuf;
+        sendbuf = nullptr;
+
+        if (!sendqueue.isempty()) {
+            sendbuf = sendqueue.get();
+            sendpos = 0;
+        }
+    }
+    ready = false;
+    io.stop();
+}
+
+void
+RecvBuf::io_cb (ev::io &w, int revents)
+{
+    int i = ::read(fd,recvbuf+recvpos, sizeof(recvbuf)-recvpos);
+    if (i <= 0) {
+        if (i == 0 || errno != EAGAIN && errno != EWOULDBLOCK) {
+            io.stop();
+            on_error_cb();
+        }
+        return;
+    }
+    recvpos += i;
+    while (true) {
+        i = on_recv_cb(recvbuf,recvpos);
+        if (i == 0) {
+            if (recvpos == sizeof(recvbuf)) {
+                io.stop();
+                on_error_cb();
+            }
+            return;
+        }
+        if (i == recvpos)
+            recvpos = 0;
+        else {
+            recvpos -= i;
+            memcpy(recvbuf,recvbuf+i,recvpos);
+        }
+    }
+
+}
+
+void
+nonblock(int fd)
+{
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags < 0)
+        return;
+    flags |= O_NONBLOCK;
+    fcntl(fd, F_SETFL, flags);
+}
+
+void
+RecvBuf::start()
+{
+    nonblock(fd);
+    io.start(fd, ev::READ);
+}
+
+void
+SendBuf::start()
+{
+    nonblock(fd);
+    io.start(fd, ev::WRITE);
+}
+
+void
+RecvBuf::stop()
+{
+    io.stop();
+}
+
+void
+SendBuf::stop()
+{
+    io.stop();
+}
+
---
+++ src/common/iobuf.h
@@ -0,0 +1,129 @@
+/*
+    EIBD eib bus access and management daemon
+    Copyright (C) 2005-2011 Martin Koegler <mkoegler@auto.tuwien.ac.at>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+/**
+ * This 
+ */
+
+#ifndef IOBUF_H
+#define IOBUF_H
+
+#include "types.h"
+#include "callbacks.h"
+#include <ev++.h>
+#include <queue.h>
+
+class SendBuf
+{
+  ev::io io;
+  void io_cb (ev::io &w, int revents);
+
+public:
+  error_cb on_error_cb;
+  void on_error() {}
+
+  SendBuf(int fd) {
+    this->fd = fd;
+    io.set<SendBuf, &SendBuf::io_cb>(this);
+    on_error_cb.set<SendBuf,&SendBuf::on_error>(this);
+  };
+
+  virtual ~SendBuf() {
+    while (!sendqueue.isempty()) {
+      const CArray *buf = sendqueue.get();
+      delete buf;
+    }
+    if (sendbuf)
+      delete sendbuf;
+  };
+
+  void start();
+  void stop();
+
+  void write(const uchar *buf, size_t len) {
+    CArray *data = new CArray(buf, len);
+    write(data);
+  }
+
+  void write(const CArray *data) {
+    sendqueue.put(data);
+    if (!ready) {
+        ready = true;
+        io.start();
+    }
+  }
+
+protected:
+  /** client connection */
+  int fd;
+
+  /** sending */
+  const CArray *sendbuf = nullptr;
+  unsigned sendpos;
+  Queue <const CArray *> sendqueue;
+  bool ready = false;
+};
+
+class RecvBuf
+{
+  ev::io io;
+  void io_cb (ev::io &w, int revents);
+
+public:
+  error_cb on_error_cb;
+  recv_cb on_recv_cb;
+
+  template<class K, void (K::*method)()>
+  void set_error_cb (K *object)
+  {
+    on_error_cb.set<K,&K::method>(object);
+  }
+
+  template<class K, void (K::*method)()>
+  void set_recv_cb (K *object)
+  {
+    on_recv_cb.set<K,&K::method>(object);
+  }
+
+  // dummy methods, to be overridden
+  void on_error() {}
+  size_t on_data(uint8_t *buf, size_t len) { return len; }
+
+  RecvBuf(int fd) {
+    this->fd = fd;
+    io.set<RecvBuf, &RecvBuf::io_cb>(this);
+    on_error_cb.set<RecvBuf,&RecvBuf::on_error>(this);
+    on_recv_cb.set<RecvBuf,&RecvBuf::on_data>(this);
+  };
+  virtual ~RecvBuf() {};
+
+  void start();
+  void stop();
+
+protected:
+  /** client connection */
+  int fd;
+
+  /** receiving */
+  uint8_t recvbuf[1024];
+  size_t recvpos = 0;
+  int len = 0; // of current block
+};
+
+#endif
