7155a69410848ff2bbfb214bdc1bfaae913ba060
Matthias Urlichs <matthias@urlichs.de>
2017-02-01 01:22:45 +0100

    Include the git version in our version string

--- tools/version.sh
+++ tools/version.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-sed -ne '1s/.*(\(.*\)-.*).*/\1/p' debian/changelog | tr -d "\n"
-git=$(git rev-parse --short HEAD 2>/dev/null)
-if [ $? = 0 ] ; then
+sed -ne '1s/.*(\(.*\)).*/\1/' -e '1s/-1$//' -e '1p' debian/changelog | tr -d "\n"
+test -d .git || exit
+git=$(git rev-parse --short HEAD)
+lgit=$(git rev-parse --short $(git rev-list -1 HEAD debian/changelog) )
+if test "$git" != "$lgit" ; then
 	echo -n ":$git"
 fi
