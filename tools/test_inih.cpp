0f5cb96c42cc74a68575e586e870e644fe5aff7e
Matthias Urlichs <matthias@urlichs.de>
2017-02-07 13:06:57 +0100

    inih test program works.

--- tools/test_inih.cpp
+++ tools/test_inih.cpp
@@ -20,21 +20,51 @@
 #include "inifile.h"
 
 #include <assert.h>
-#include <stdio.h>
+#include <iostream>
 
 main(int argc, const char *argv[])
 {
-  if(argc != 2) 
+  if(argc < 2) 
     {
-      fprintf(stderr, "Usage: %s testfile\n", argv[0]);
+      std::cerr << "Usage: " << argv[0] << "goodfile badfile…" << std::endl;
       exit(1);
     }
 
   IniData i;
-  i.parse(argv[1]);
+  int errl = i.parse(argv[1]);
+  if (errl)
+    {
+      std::cerr << "Parse error in '" << argv[1] << "', line" << errl << ". Stop." <<std::endl;
+      exit(2);
+    }
 
   IniSection &a = i["main"];
-  assert (a["foo"] == "bar");
-  assert (a.value("baz",99) == 42);
-  assert (a.value("duh",false));
+  if (a["foo"] != "bar")
+    {
+      std::cerr << "in section 'main': foo should be 'bar' but is '" << a["foo"] << "'" << std::endl;
+      exit(1);
+    }
+  if (a.value("baz",99) != 42)
+    {
+      std::cerr << "in section 'main': baz should be 42 but is " << a.value("baz",88) << std::endl;
+      exit(1);
+    }
+  if (!a.value("duh",false))
+    {
+      std::cerr << "in section 'main': duh should be true" << std::endl;
+      exit(1);
+    };
+
+  std::cerr << "'Good' test completed. Ignore the following errors." << std::endl;
+  while(argc > 2)
+    {
+      IniData ix;
+      if (!ix.parse(argv[--argc])) 
+        {
+          std::cerr << "file '" << argv[argc] << "should not parse cleanly" << std::endl;
+          exit(1);
+        }
+    }
+  std::cerr << "All tests completed correctly." << std::endl;
+  exit(0);
 }
