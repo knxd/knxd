8b1c736c9a3ecb55fc68f8384435ccc0e2c484ed
Matthias Urlichs <matthias@urlichs.de>
2017-03-12 12:08:53 +0100

    … and set reuseaddr so that more than one source connection can be used
    if a source port has been passed in

--- src/backend/eibnettunnel.cpp
+++ src/backend/eibnettunnel.cpp
@@ -48,7 +48,7 @@ EIBNetIPTunnel::EIBNetIPTunnel (const char *dest, int port, int sport,
   raddr.sin_port = htons (sport);
   NAT = false;
   dataport = Dataport;
-  sock = new EIBNetIPSocket (raddr, 0, t);
+  sock = new EIBNetIPSocket (raddr, (sport != 0), t);
   if (!sock->init ())
     {
       delete sock;
