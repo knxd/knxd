knxd/knxd

commit ddc0e8552ee19bb8d7df71631508548c7ffb6822

eibread-cgi: old last-update reader: wraparound handled wrongly

src/libserver/groupcache.cpp
@@ -354,10 +354,11 @@ GroupCache::LastUpdates (uint16_t start, uint8_t Timeout,
     GCLastCallback cb, ClientConnPtr cc)
 {
   uint32_t st;
-  if (seq > 0xFFFF && start > (st&0xFFFF))
-    st = (seq&~0xFFFF) - 0x10000 + start;
+  // counter wraparound
+  st = (seq&~0xFFFF) + start;
+  if (st > seq)
+    st -= 0x10000;
   else
-    st = start;
   new GCTracker(this, st, Timeout, cb,cc);
 }
 

commit 2ab6f5842ece3ac9a7ea02231b49e288b998cf7b

Fix GroupCache::LastUpdates to always enumerate changes
Bug was introduced in ddc0e8552ee19bb8d7df71631508548c7ffb6822
(version 0.12.11-1) when the code in the else branch was removed
without removing the else keyword.

Signed-off-by: Thomas Dallmair <dallmair@users.noreply.github.com>

src/libserver/groupcache.cpp
@@ -374,8 +374,7 @@ GroupCache::LastUpdates (uint16_t start, uint8_t Timeout,
   st = (seq&~0xFFFF) + start;
   if (st > seq)
     st -= 0x10000;
-  else
-    new GCTracker(this, st, Timeout, cb,cc);
+  new GCTracker(this, st, Timeout, cb,cc);
 }
 
 void