5d268babec9fdbe911503b09bca658b846508e4e
Sven Fischer <sven@leiderfischer.de>
2019-02-08 09:13:28 +0100

    Corrected fmt external project definition

--- cmake/ExternalFmt.cmake
+++ cmake/ExternalFmt.cmake
@@ -8,7 +8,7 @@ endif()
 include(ExternalProject)
 
 ExternalProject_Add(
-  fmtlib
+  external_fmt
   GIT_REPOSITORY https://github.com/fmtlib/fmt
   CMAKE_ARGS -DFMT_TEST=OFF
   INSTALL_COMMAND ""
@@ -17,22 +17,22 @@ ExternalProject_Add(
   LOG_BUILD ON)
 
 # Specify include dir
-ExternalProject_Get_Property(fmtlib source_dir)
+ExternalProject_Get_Property(external_fmt source_dir)
 set(FMT_INCLUDE_DIRS ${source_dir} CACHE PATH "" FORCE)
 
 # Library
-ExternalProject_Get_Property(fmtlib binary_dir)
+ExternalProject_Get_Property(external_fmt binary_dir)
 set(FMT_LIBRARY_PATH ${binary_dir}/fmt/${CMAKE_FIND_LIBRARY_PREFIXES}fmt.a CACHE FILEPATH "" FORCE)
 
 set(FMT_LIBRARY fmt)
-add_library(${FMT_LIBRARY} UNKNOWN IMPORTED)
-set_target_properties(${FMT_LIBRARY} PROPERTIES
-  IMPORTED_LOCATION ${FMT_LIBRARY_PATH}
-  INTERFACE_COMPILE_OPTIONS "-std=c++11"
-  )
-add_dependencies(${FMT_LIBRARY} fmtlib)
+add_library(fmt STATIC IMPORTED)
+set_property(TARGET fmt PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${FMT_INCLUDE_DIRS})
+set_property(TARGET fmt PROPERTY IMPORTED_LOCATION ${FMT_LIBRARY_PATH})
+set_property(TARGET fmt PROPERTY INTERFACE_COMPILE_OPTIONS "-std=c++11")
 
-set(FMT_LIBRARIES ${FMT_LIBRARY})
+add_dependencies(fmt external_fmt)
+
+set(FMT_LIBRARIES fmt)
 
 mark_as_advanced(
   FMT_INCLUDE_DIRS
