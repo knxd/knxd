8c3f7429afc3b61b8fe377c39f0c2eee6043f134
Tobias Lorenz <tobias.lorenz@gmx.net>
2019-10-15 22:16:46 +0200

    BCU_LOAD_RESULT: Replace defines with enum
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

--- src/common/eibloadresult.h
+++ src/common/eibloadresult.h
@@ -28,69 +28,71 @@
 #ifndef EIB_LOAD_RESULT_H
 #define EIB_LOAD_RESULT_H
 
-#define IMG_UNKNOWN_ERROR             0
-#define IMG_UNRECOG_FORMAT            1
-#define IMG_INVALID_FORMAT            2
-#define IMG_NO_BCUTYPE                3
-#define IMG_UNKNOWN_BCUTYPE           4
-#define IMG_NO_CODE                   5
-#define IMG_NO_SIZE                   6
-#define IMG_LODATA_OVERFLOW           7
-#define IMG_HIDATA_OVERFLOW           8
-#define IMG_TEXT_OVERFLOW             9
-#define IMG_NO_ADDRESS               10
-#define IMG_WRONG_SIZE               11
-#define IMG_IMAGE_LOADABLE           12
-#define IMG_NO_DEVICE_CONNECTION     13
-#define IMG_MASK_READ_FAILED         14
-#define IMG_WRONG_MASK_VERSION       15
-#define IMG_CLEAR_ERROR              16
-#define IMG_RESET_ADDR_TAB           17
-#define IMG_LOAD_HEADER              18
-#define IMG_LOAD_MAIN                19
-#define IMG_ZERO_RAM                 20
-#define IMG_FINALIZE_ADDR_TAB        21
-#define IMG_PREPARE_RUN              22
-#define IMG_RESTART                  23
-#define IMG_LOADED                   24
-#define IMG_NO_START                 25
-#define IMG_WRONG_ADDRTAB            26
-#define IMG_ADDRTAB_OVERFLOW         27
-#define IMG_OVERLAP_ASSOCTAB         28
-#define IMG_OVERLAP_TEXT             29
-#define IMG_NEGATIV_TEXT_SIZE        30
-#define IMG_OVERLAP_PARAM            31
-#define IMG_OVERLAP_EEPROM           32
-#define IMG_OBJTAB_OVERFLOW          33
-#define IMG_WRONG_LOADCTL            34
-#define IMG_UNLOAD_ADDR              35
-#define IMG_UNLOAD_ASSOC             36
-#define IMG_UNLOAD_PROG              37
-#define IMG_LOAD_ADDR                38
-#define IMG_WRITE_ADDR               39
-#define IMG_SET_ADDR                 40
-#define IMG_FINISH_ADDR              41
-#define IMG_LOAD_ASSOC               42
-#define IMG_WRITE_ASSOC              43
-#define IMG_SET_ASSOC                44
-#define IMG_FINISH_ASSOC             45
-#define IMG_LOAD_PROG                46
-#define IMG_ALLOC_LORAM              47
-#define IMG_ALLOC_HIRAM              48
-#define IMG_ALLOC_INIT               49
-#define IMG_ALLOC_RO                 50
-#define IMG_ALLOC_EEPROM             51
-#define IMG_ALLOC_PARAM              52
-#define IMG_SET_PROG                 53
-#define IMG_SET_TASK_PTR             54
-#define IMG_SET_OBJ                  55
-#define IMG_SET_TASK2                56
-#define IMG_FINISH_PROC              57
-#define IMG_WRONG_CHECKLIM           58
-#define IMG_INVALID_KEY              59
-#define IMG_AUTHORIZATION_FAILED     60
-#define IMG_KEY_WRITE                61
-
-typedef int BCU_LOAD_RESULT;
+typedef enum
+{
+  IMG_UNKNOWN_ERROR           =  0,
+  IMG_UNRECOG_FORMAT          =  1,
+  IMG_INVALID_FORMAT          =  2,
+  IMG_NO_BCUTYPE              =  3,
+  IMG_UNKNOWN_BCUTYPE         =  4,
+  IMG_NO_CODE                 =  5,
+  IMG_NO_SIZE                 =  6,
+  IMG_LODATA_OVERFLOW         =  7,
+  IMG_HIDATA_OVERFLOW         =  8,
+  IMG_TEXT_OVERFLOW           =  9,
+  IMG_NO_ADDRESS              = 10,
+  IMG_WRONG_SIZE              = 11,
+  IMG_IMAGE_LOADABLE          = 12,
+  IMG_NO_DEVICE_CONNECTION    = 13,
+  IMG_MASK_READ_FAILED        = 14,
+  IMG_WRONG_MASK_VERSION      = 15,
+  IMG_CLEAR_ERROR             = 16,
+  IMG_RESET_ADDR_TAB          = 17,
+  IMG_LOAD_HEADER             = 18,
+  IMG_LOAD_MAIN               = 19,
+  IMG_ZERO_RAM                = 20,
+  IMG_FINALIZE_ADDR_TAB       = 21,
+  IMG_PREPARE_RUN             = 22,
+  IMG_RESTART                 = 23,
+  IMG_LOADED                  = 24,
+  IMG_NO_START                = 25,
+  IMG_WRONG_ADDRTAB           = 26,
+  IMG_ADDRTAB_OVERFLOW        = 27,
+  IMG_OVERLAP_ASSOCTAB        = 28,
+  IMG_OVERLAP_TEXT            = 29,
+  IMG_NEGATIV_TEXT_SIZE       = 30,
+  IMG_OVERLAP_PARAM           = 31,
+  IMG_OVERLAP_EEPROM          = 32,
+  IMG_OBJTAB_OVERFLOW         = 33,
+  IMG_WRONG_LOADCTL           = 34,
+  IMG_UNLOAD_ADDR             = 35,
+  IMG_UNLOAD_ASSOC            = 36,
+  IMG_UNLOAD_PROG             = 37,
+  IMG_LOAD_ADDR               = 38,
+  IMG_WRITE_ADDR              = 39,
+  IMG_SET_ADDR                = 40,
+  IMG_FINISH_ADDR             = 41,
+  IMG_LOAD_ASSOC              = 42,
+  IMG_WRITE_ASSOC             = 43,
+  IMG_SET_ASSOC               = 44,
+  IMG_FINISH_ASSOC            = 45,
+  IMG_LOAD_PROG               = 46,
+  IMG_ALLOC_LORAM             = 47,
+  IMG_ALLOC_HIRAM             = 48,
+  IMG_ALLOC_INIT              = 49,
+  IMG_ALLOC_RO                = 50,
+  IMG_ALLOC_EEPROM            = 51,
+  IMG_ALLOC_PARAM             = 52,
+  IMG_SET_PROG                = 53,
+  IMG_SET_TASK_PTR            = 54,
+  IMG_SET_OBJ                 = 55,
+  IMG_SET_TASK2               = 56,
+  IMG_FINISH_PROC             = 57,
+  IMG_WRONG_CHECKLIM          = 58,
+  IMG_INVALID_KEY             = 59,
+  IMG_AUTHORIZATION_FAILED    = 60,
+  IMG_KEY_WRITE               = 61,
+}
+BCU_LOAD_RESULT;
 
 #endif
