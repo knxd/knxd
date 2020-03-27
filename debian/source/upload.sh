d3e953899cf202ddad3efac521f1f2a37080fa71
Matthias Urlichs <matthias@urlichs.de>
2020-03-27 13:03:40 +0100

    Add upload script (via salsa)

---
+++ debian/source/upload.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -ex
+B="$(git describe --tags --exact-match master)"
+test -n "$B"
+git push salsa $B
+
+git checkout deb
+
+D="$(git describe --tags --exact-match deb)"
+if test -z "$D" ; then
+	git-debpush -u AFD79782F3BAEC020B28A19F72CF8E5E25B4C293 --upstream "$B" --remote salsa --quilt=baredebian
+	D="$(git describe --tags --exact-match deb)"
+fi
+test -n "$D"
+
+T=$(tempfile)
+rm -f $T
+mkdir $T
+cd $T
+trap 'cd /; cat overall.log; rm -rf $T' 0 1 2 15
+
+DGIT_DRS_EMAIL_NOREPLY=smurf@debian.org dgit-repos-server debian . /usr/share/keyrings/debian-keyring.gpg,a --tag2upload https://salsa.debian.org/smurf/knxd.git "$D"
