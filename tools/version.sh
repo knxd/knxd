9ca7daf4b5a4b9ecfb3ddf24515f5d42c954ee4f
Matthias Urlichs <matthias@urlichs.de>
2021-01-13 12:33:57 +0100

    More versioning nonsense

--- tools/version.sh
+++ tools/version.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-test -d .git || exit
-# git describe --tags
-git log --format=format:%D | perl -ne 'next unless s#.*tag: ##; s#,.*##; next if m#/#; print; exit;'
+dpkg-parsechangelog -SVersion | sed -e 's/.*://' -e 's/-.*//'
