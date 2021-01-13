cc3b33195add24c4bd853c0f1a8528b13eca2905
Matthias Urlichs <matthias@urlichs.de>
2021-01-13 17:22:09 +0100

    Another attempt

--- tools/version.sh
+++ tools/version.sh
@@ -2,4 +2,12 @@
 
 test -d .git || exit
 # git describe --tags
+
+# Sorry about the Debian nonsense in here but the build system doesn't
+# patch the file early enough and overriding it is a PITA
+if test -s debian/changelog ; then
+    dpkg-parsechangelog -SVersion | sed -e 's/.*://' -e 's/-.*//'
+    exit
+fi
+
 git log --format=format:%D | perl -ne 'next unless s#.*tag: ##; s#,.*##; next if m#/#; print; exit;'
