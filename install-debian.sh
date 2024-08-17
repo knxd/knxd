abb5bc37d05c978ca7cf332af637a5f3b87fcbd3
Greig Sheridan <greiginsydney@users.noreply.github.com>
2024-08-17 16:45:07 +1000

    Added --yes to apt for unattended builds

--- install-debian.sh
+++ install-debian.sh
@@ -6,7 +6,7 @@ export LC_ALL=C.UTF-8
 cd "$(dirname "$0")"
 
 : 1 install tools, minimal variant
-sudo apt-get install --no-install-recommends build-essential devscripts equivs
+sudo apt-get install --no-install-recommends build-essential devscripts equivs --yes
 
 : 2 auto-install packages required for building knxd
 sudo mk-build-deps --install --tool='apt-get --no-install-recommends --yes --allow-unauthenticated' debian/control
@@ -21,5 +21,5 @@ cd ..
 sudo dpkg -i knxd_*.deb knxd-tools_*.deb
 
 : 5 Clean up. Optional. Remove this if you want to rebuild soon-ish.
-sudo apt remove --autoremove knxd-build-deps
+sudo apt remove --autoremove knxd-build-deps --yes
 
