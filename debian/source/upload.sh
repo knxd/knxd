e689faa133657072c63c505fc6654c0e962ed90e
Matthias Urlichs <matthias@urlichs.de>
2024-12-17 22:27:00 +0100

    Fix upload script's key handling

--- debian/source/upload.sh
+++ debian/source/upload.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -ex
+if ! test -v DEBSIGN_KEYID && ! test -v KEYID ; then
+	echo [DEBSIGN_]KEYID not set
+	exit 1
+fi
+
 D="$(git describe --tags --exact-match main)"
 git push salsa main $D
 
@@ -10,7 +15,7 @@ D="$(git describe --tags --exact-match main)"
 test -s ../knxd_$D.orig.tar.gz || \
 wget -O ../knxd_$D.orig.tar.gz https://salsa.debian.org/smurf/knxd/-/archive/$D/knxd-0.$D.tar.gz
 dgit --dpm --ch:-sa --quilt=single build-source
-debsign -S -k${KEYID:-AFD79782F3BAEC020B28A19F72CF8E5E25B4C293}
+debsign -S -k${DEBSIGN_KEYID:-${KEYID}}
 git push salsa debian
 
 # DGIT_DRS_EMAIL_NOREPLY=smurf@debian.org dgit-repos-server debian . /usr/share/keyrings/debian-keyring.gpg,a --tag2upload https://salsa.debian.org/smurf/knxd.git "$D"
