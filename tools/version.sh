57b59bf961a2812613cebbb953bd22dbeedd36e9
Thomas Dallmair <dev@thomas-dallmair.de>
2024-12-29 01:28:01 +0100

    Also check for existence of `dpkg-parsechangelog`
    Now that every push to GitHub triggers a workflow, even pushes to
    `debian` (or any newly created branches from it) do build on all
    distributions. Therefore, merely checking existence of the file is not
    sufficient to determine whether to use this or that approach for
    detecting the version.

--- tools/version.sh
+++ tools/version.sh
@@ -2,7 +2,7 @@
 
 # Sorry about the Debian nonsense in here but the build system doesn't
 # patch the file early enough and overriding it is a PITA
-if test -s debian/changelog ; then
+if test -s debian/changelog && which dpkg-parsechangelog >/dev/null ; then
     dpkg-parsechangelog -SVersion | sed -e 's/.*://' -e 's/-.*//'
     exit
 fi
