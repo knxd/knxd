e053cb84905a71dea01e093260beb2243c47e48b
Matthias Urlichs <matthias@urlichs.de>
2024-12-31 14:06:04 +0100

    Check for auto-remove-able packages in debian install script.
    Closes #614.

--- install-debian.sh
+++ install-debian.sh
@@ -5,6 +5,19 @@ export LC_ALL=C.UTF-8
 
 cd "$(dirname "$0")"
 
+: 0 make sure the system is clean
+
+if ! sudo apt-get autoremove --assume-no ; then
+	set +x
+	echo "DANGER: this script cleans up after itself, but you have packages"
+	echo "marked for auto-removal which we would remove from your system."
+	echo ""
+	echo -n "Enter 'y' to proceed: "
+	read y
+	test y = "$y" || exit 1
+	set -x
+fi
+
 : 1 install tools, minimal variant
 sudo apt-get install --no-install-recommends build-essential devscripts equivs --yes
 
